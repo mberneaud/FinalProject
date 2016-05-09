7e40b202ea4fcd788fdb271939e7b807ffcfc8a0
Malte Berneaud <malte.berneaud@gmail.com>
2016-05-09 19:55:46 +0200

    started directories, files and branches

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# FinalProject
-Contains the research paper and the website for the Introduction to Collaborative Data Science course at the Hertie School
+# Final Project
+
+This repo contains [Jonas Markgraf's](https://github.com/jmarkgraf) and [Malte Berneaud-Kötz's](https://github.com/mberneaud) final paper and website of our research project in the Collaborative Social Science Data Analysis course at the Hertie School of Governance. 
+
+**Instrucutor**: Christopher Gandrud
+
+## Note regarding the data sets used in the analysis
+
+This repo does not contain the necessary data sets needed to reproduce the analysis, as one data set is hand-collected and not intended for publication at this stage and the other one has to be bought at the Bavarian statistical office. 
+
+As we do not intent to publish the data used in the analysis publicly, it needs to be manually inserted into the `data/`  directory. Otherwise, the RMarkdown will not render correctly as one of the earlier code chunks will throw an error right away. 
+
+
+**Please Note**: The paper is based on and a part of a research project by Guillermo Rosas (Washington University in St. Louis; grosas(at)wustl.edu) and Jonas Markgraf (Hertie School of Governance; markgraf(at)hertie-school.org).
---
+++ paper/standalone_R_code.R
@@ -0,0 +1,172 @@
+# R code for the cleaning of the data, creation of additional variables, subsetting and lagging of variables
+# Author: Malte Berneaud
+# Date: 30.04.2016
+
+# Packages needed for the execution of this Rmd are listed in include.packages and checked against the installed packages on the machine executing the code. If they are not installed, they will be installed automatically.
+include.packages <- c("dplyr", "ggplot2", "stringr", "readxl", "DataCombine", "texreg",
+                      "stargazer", "MASS")
+needed.packages <- include.packages[!(include.packages %in% installed.packages()[, "Package"])]
+if(length(needed.packages)) install.packages(needed.packages, 
+                                             repos = "https://cran.uni-muenster.de/")
+
+lapply(include.packages, library, character.only = TRUE)  # loading all packages at once
+
+
+# Loading data ------------------------------------------------------------
+
+# Sparkassen Board Membership data
+SparkassenBoard <- read_excel("data/BankBoards_Bavaria_RImport.xlsx",
+                              sheet = "BoardComp")
+## define variable class:
+SparkassenBoard$Incumbent <- as.character(SparkassenBoard$Incumbent)
+
+# Municipal Election data
+MayorElection <- read_excel("data/MayorElectionData.xlsx",
+                            sheet = "Bgm_OB")
+
+
+# Cleaning Sparkassen data set --------------------------------------------
+
+# renaming variables
+oldnames <- c("^name$", "^political...employee$", "^governing...opposition$", "retired.1.yes.",
+              "Name_CountyMunicipality", "Type_CountyMunicipality")
+newnames <- c("NameCandidate1", "political_employee", "governing_opposition", "retired",
+              "NameMunicipality", "type_county_municipality")
+for (i in seq_along(oldnames)) {
+  names(SparkassenBoard) <- gsub(oldnames[i], newnames[i], names(SparkassenBoard))
+}
+
+# trimming whitespace:
+SparkassenBoard$NameMunicipality <- str_trim(SparkassenBoard$NameMunicipality)
+SparkassenBoard$NameCandidate1 <- str_trim(SparkassenBoard$NameCandidate1)
+
+# creating variable for top position
+SparkassenBoard$TopPosition <- 0
+SparkassenBoard$TopPosition[SparkassenBoard$chair != "no"] <- 1
+SparkassenBoard$TopPosition <- as.character(SparkassenBoard$TopPosition)
+
+# Create sub-dataframes
+## :unique banks
+SparkassenBoard_UniqueBanks <- unique(SparkassenBoard[ ,c("bank_ID", "bank_name", 
+                                                          "federal_state", "city", 
+                                                          "board_size")])
+
+## :Top Positions only
+SparkassenBoard_TopPositions <- subset(SparkassenBoard, TopPosition == 1)
+
+
+# Cleaning municipality election data set ---------------------------------
+
+# removing newlines
+names(MayorElection) <- str_replace_all(names(MayorElection), "[\r\n]" , "")  
+# removing punctuation
+names(MayorElection) <- gsub("[[:punct:]]", "", names(MayorElection))
+# removing whitespace
+names(MayorElection) <- str_trim(names(MayorElection))
+# correcting "Geschlecht"
+names(MayorElection) <- gsub("^Geschl", "Geschlecht", names(MayorElection))
+
+# Changing label names
+newnames <- c("IDMunicipality","NameMunicipality", "ElectionDate", "RunOffNecessary", "ElectionType", "ProfPolitician", "NumberEligVoter", "NumberVoters", "InvalBallots", "ValBallots", "NameCandidate1")
+
+names(MayorElection)[1:11] <- newnames
+
+names(MayorElection) <- gsub("gültigeStimmen 1", "VotesCandidate1", names(MayorElection))
+names(MayorElection) <- gsub("Stimmen für nicht genannte Bewerber zusammen", "VotesRest", names(MayorElection))
+
+# removing empty column 85
+MayorElection[, 85] <- NULL
+
+# Cleaning of gender strings
+MayorElection$Geschlecht1 <- str_trim(MayorElection$Geschlecht1)
+
+# cleaning of party names
+MayorElection$MayorParty <- MayorElection$"Wahlvorschlag 1"
+MayorElection$MayorParty <- str_trim(MayorElection$MayorParty)
+# reduce party to main party only
+MayorElection$MayorParty <- sub("/.*","", MayorElection$MayorParty)
+
+
+
+# Cleaning the party names for bar plotting
+MayorElection$CleanParty <- NA
+for(i in 1:nrow(MayorElection)) {
+  if(is.na(MayorElection$MayorParty[i])) {
+    next
+  }
+  if(grepl("CSU", MayorElection$MayorParty[i])) {
+    MayorElection$CleanParty[i] <- "CSU"
+  }else if(grepl("SPD", MayorElection$MayorParty[i])) {
+    MayorElection$CleanParty[i] <- "SPD"
+  } else {
+    MayorElection$CleanParty[i] <- "Other"
+  }
+}
+
+
+# Creating additional variables -------------------------------------------
+
+# Extracting Phd titles
+MayorElection$Phd <- NA
+MayorElection$Phd <- ifelse(grepl("Dr.", MayorElection$NameCandidate1), 1, 0)
+MayorElection$NameCandidate1 <- gsub(" Dr.", "", MayorElection$NameCandidate1)
+MayorElection$NameCandidate1 <- gsub(" jur.", "", MayorElection$NameCandidate1)
+
+# creating the total number of terms for each mayor
+tally <- dplyr::tally(group_by(MayorElection, NameCandidate1))
+names(tally)[2] <- "TotalTerms"
+#merging that back into the data frame
+MayorElection <- merge(MayorElection, tally)
+
+# Extracting years from the election date character string
+MayorElection$Year <- strtrim(MayorElection$ElectionDate, 4L)
+MayorElection$Year <- as.integer(MayorElection$Year)  ## coercing year variable to an integer
+
+# Dummy variable for contested elections
+MayorElection$Contested <- ifelse(nchar(MayorElection$`Name 2Nachname Titel Vorname`)>2, 1, 0)
+
+# Calculating votes shares from the data
+MayorElection$VoteShareWinner <-  (MayorElection$VotesCandidate1 / MayorElection$ValBallots) * 100
+
+
+# Merging data sets -------------------------------------------------------
+# Ordering the data by municipality
+MayorElection$IDMunicipality <- as.integer(MayorElection$IDMunicipality)
+MayorElection <- arrange(MayorElection, IDMunicipality, Year)
+
+
+##: variable for board membership
+MayorElection$SparkassenMember <- is.element(MayorElection$NameCandidate1, unique(SparkassenBoard$NameCandidate1))
+
+##: variable for board membership of incumbent
+#library(DataCombine) # I moved the loading of the library up in the initiatl code which checks whether all necessary packages are installed, installs missings and then load them (named: "fetching_library") (MB)
+MayorElection <- slide(MayorElection, Var = "SparkassenMember",
+                       NewVar = "IncumbentSparkassenMember")
+
+# Subsetting the data set (part 1) -------------------------------------------------
+
+# subsetting by election type; excluding run-off elections
+MayorElection <- subset(MayorElection, ElectionType != 3)
+
+# Subsetting to contested elections only
+MayorElection <- subset(MayorElection, Contested == 1)
+
+# excluding mayors who did not run again (likely because of retirement)
+
+
+# creating lagged variables -----------------------------------------------
+## Lagged DV
+# creating lagged incumbency for the calculation of the re-election binary variable
+MayorElection <- slide(MayorElection, Var = "NameCandidate1", TimeVar = "ElectionDate", NewVar = "L.NameCandidate1")
+MayorElection$Reelection <- ifelse(MayorElection$NameCandidate1 == MayorElection$L.NameCandidate1, 1, 0)
+
+# Lagging variables for analysis, lag of SparkassenMembership is already implemented above. Lag variable is called "IncumbentSparkassenMember"
+MayorElection <- slide(MayorElection, Var = "Geschlecht1", TimeVar = "ElectionDate", NewVar = "L.Geschlecht1")
+MayorElection <- slide(MayorElection, Var = "VoteShareWinner", TimeVar = "ElectionDate", NewVar = "L.VoteShareWinner")
+
+# Subsetting for years ----------------------------------------------------
+MayorElection <- subset(MayorElection, Year >= 2006)
+
+
+
+
